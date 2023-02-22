eb651c6e2c42a0cc6e1392f5632b1717a05ceaf7
Joanne Greenhalgh <joanne.greenhalgh@roq.co.uk>
2023-02-22 10:50:18 +0000

    Added tests to test number of lives

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -12,4 +12,63 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	
+	@Test
+	public void testFeedingOnce() {
+		Cat cat = new Cat();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 30);
+	}
+	
+	@Test
+	public void testFeedingUntilNotHungry() {
+		Cat cat = new Cat();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+	}
+	
+	@Test
+	public void testRuns() {
+		Cat cat = new Cat();
+		cat.runs();
+		assertEquals(cat.howHungry(), 80);
+	}
+	
+	@Test
+	public void testFeedAndRun() {
+		Cat cat = new Cat();
+		cat.runs();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 60);
+	}
+	
+	@Test
+	public void testCatLives() {
+		Cat cat = new Cat();
+		cat.runs();
+		cat.runs();
+		assertTrue(cat.isAlive());
+	}
+	
+	@Test
+	public void testCatDiesAfterNineDeaths() {
+		Cat cat = new Cat();
+		for (int i = 0; i < 9; i++) {
+			cat.runs();
+			cat.runs();
+		}
+		assertTrue(!cat.isAlive());
+	}
+	
+	@Test
+	public void testCatAliveAfterEightDeaths() {
+		Cat cat = new Cat();
+		for (int i = 0; i < 8; i++) {
+			cat.runs();
+			cat.runs();
+		}
+		assertTrue(cat.isAlive());
+	}
 }
